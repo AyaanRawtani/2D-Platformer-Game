29ad21473eae6df3f363b3482bec40a596ba5fd7
AyaanRawtani <86621674+AyaanRawtani@users.noreply.github.com>
2025-04-01 23:18:17 +0530

    Feature 3 tile map (#4)
    * Added rigidbody+collider+HorizontalMovement
    
    Added PlayerController scirpt with left n right movement
    
    * Added Crouch and Jump
    
    Jumping breaks the run and idle anims
    Crouching isn't working as intended either
    
    * Added Basic movement
    
    * added isGrounded bool
    
    the jump anim is getting played if I press space mid jump
    
    * Added tag for platform
    
    Still couldn't fix the problem where player is jumping mid jump
    
    * Added TileMap
    
    Added tilemap and made the cam follow player
    
    * applied overrides to Player prefab
    
    * Added LevelOver script and Trigger
    
    on trigger event to tell when lvl is over
    
    * Added PlayerDeath upon falling
    
    added a trigger below the level and a PLayerDeath scrript
    
    * Playing with jump number and trying to fix anims
    
    * Fixed crouch

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private BoxCollider2D boxCol;
+
+    private Vector2 boxColInitSize;
+    private Vector2 boxColInitOffset;
+
+    private Rigidbody2D rb2d;
+
+    public float playerSpeed;
+    public float playerJump;
+
+    private bool isGrounded = false;
+
+    private void Awake()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        boxColInitSize = boxCol.size;
+        boxColInitOffset = boxCol.offset;
+    }
+
+    public void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
+
+        PlayMovementAnimation(horizontal, vertical);
+        MoveCharacter(horizontal, vertical );
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            Crouch(true);
+        }
+        else
+        {
+            Crouch(false);
+        }
+    }
+
+    private void PlayMovementAnimation(float horizontal , float vertical)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+        Vector3 scale = transform.localScale;
+        if (horizontal < 0)                                                      //flipping the run dir
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (horizontal > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+
+       
+        if (vertical > 0 && isGrounded == true )                                                              //jump
+        {
+            animator.SetBool("Jump", true);
+
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
+    }
+
+    private void MoveCharacter(float horizontal, float vertical)
+    {
+        //horizontal - left-right
+        Vector3 position = transform.position;
+        position.x += horizontal * playerSpeed * Time.deltaTime;
+        transform.position = position;
+
+        //vert- jump
+        if (vertical > 0 && isGrounded == true)
+        { 
+            rb2d.AddForce(new Vector2(0f, playerJump), ForceMode2D.Force);                
+        }
+
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if(other.transform.tag == "platform")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.transform.tag == "platform")
+        {  isGrounded = false;}
+    }
+
+    public void Crouch(bool crouch)
+    {
+        if (crouch == true)
+        {
+            float offX = 0.03588617f;
+            float offY = 0.5778199f;
+
+            float sizeX = 0.6468706f;
+            float sizeY = 1.226415f;
+
+            boxCol.size = new Vector2(sizeX, sizeY);
+            boxCol.offset = new Vector2(offX, offY);
+            
+        }
+        else
+        {
+            boxCol.size = boxColInitSize;
+            boxCol.offset = boxColInitOffset;
+            
+        }
+        animator.SetBool("Crouch", crouch);
+    }
+    
+
+}
